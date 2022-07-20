0964c534570123394297b6ff03a9457ef2902bf0
fechan <work.fechan@gmail.com>
2022-07-20 01:30:49 -0700

    revery ugly change

--- machineUtils.lua
+++ machineUtils.lua
@@ -1,11 +1,6 @@
 local function findFirst(driver, type)
-    local machine = nil
-    -- this for loop is inelegant but I couldn't get it to work otherwise??
-    for _, periph in pairs({peripheral.find(type)}) do
-        machine = driver:new(nil, periph)
-        break
-    end
-    return machine
+    local first = peripheral.find(type)
+    return driver:new(nil, first)
 end
 
 return { findFirst = findFirst }
