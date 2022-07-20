2dea956da1f86a414b249bf077aea983917058d8
fechan <work.fechan@gmail.com>
2022-07-20 00:10:59 -0700

    indent with 2 spaces

--- inventoryUtils.lua
+++ inventoryUtils.lua
@@ -1,27 +1,30 @@
 -- optional: itemName (any), limit (inf), toSlot (any)
 local function transfer (from, to, itemName, limit, toSlot)
-    local toName = peripheral.getName(to.peripheral)
-    local remaining = limit or math.huge
-    local totalTransferred = 0
-    for fromSlot,item in pairs(from.peripheral.list()) do
-        if (item.name == (itemName or item.name)) and (remaining) > 0 then
-            local transferred
-            if remaining == math.huge then
-                transferred = from.peripheral.pushItems(toName, fromSlot, nil, toSlot)
-            else
-                transferred = from.peripheral.pushItems(toName, fromSlot, remaining, toSlot)
-            end
-            totalTransferred = totalTransferred + transferred
-            remaining = remaining - transferred
-        end
+  local toName = peripheral.getName(to.peripheral)
+  local remaining = limit or math.huge
+  local totalTransferred = 0
+  for fromSlot,item in pairs(from.peripheral.list()) do
+    if (item.name == (itemName or item.name)) and (remaining) > 0 then
+      local transferred
+      if remaining == math.huge then
+        transferred = from.peripheral.pushItems(toName, fromSlot, nil, toSlot)
+      else
+        transferred = from.peripheral.pushItems(toName, fromSlot, remaining, toSlot)
+      end
+      totalTransferred = totalTransferred + transferred
+      remaining = remaining - transferred
     end
-    return totalTransferred
+  end
+  return totalTransferred
 end
 
 -- optional: limit (inf)
 local function transferFromSlot (from, to, fromSlot, limit)
-    local toName = peripheral.getName(to.peripheral)
-    return from.peripheral.pushItems(toName, fromSlot, limit)
+  local toName = peripheral.getName(to.peripheral)
+  return from.peripheral.pushItems(toName, fromSlot, limit)
 end
 
-return {transfer = transfer, transferFromSlot = transferFromSlot}
+return {
+  transfer = transfer,
+  transferFromSlot = transferFromSlot
+}
