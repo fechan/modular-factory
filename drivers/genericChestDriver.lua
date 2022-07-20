cb66ced3780ce714236da4e06742db43509a62b6
fechan <work.fechan@gmail.com>
2022-07-20 02:01:20 -0700

    indent function content

--- drivers/genericChestDriver.lua
+++ drivers/genericChestDriver.lua
@@ -1,12 +1,12 @@
 GenericChest = {}
 
 function GenericChest:new (o, peripheral)
-o = o or {}
-setmetatable(o, self)
-self.__index = self
+  o = o or {}
+  setmetatable(o, self)
+  self.__index = self
 
-self.peripheral = peripheral
-return o
+  self.peripheral = peripheral
+  return o
 end
 
 return {GenericChest = GenericChest}
