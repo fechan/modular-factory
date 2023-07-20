e292f16d8d11d85c0aff7ef1050152114bbd52b5
fechan <work.fechan@gmail.com>
2023-07-20 15:43:41 -0700

    move examples because importing is weird

--- example_precision_mechanism.lua
+++ example_precision_mechanism.lua
@@ -7,13 +7,29 @@ local myStorage = Chest:new(peripheral.wrap("minecraft:chest_0"))
 local myFactory = Factory:new(myStorage)
 
 local mySequencedAssembler = SequencedAssembly:new(
-  peripheral.find("create:deployer"),
-  peripheral.find("create:depot"),
+  peripheral.wrap("create:deployer_0"),
+  peripheral.wrap("create:depot_0"),
   peripheral.wrap("minecraft:chest_2")
 )
 myFactory:addMachine(mySequencedAssembler)
 
+local mySequencedAssembler2 = SequencedAssembly:new(
+  peripheral.wrap("create:deployer_1"),
+  peripheral.wrap("create:depot_1"),
+  peripheral.wrap("minecraft:chest_3")
+)
+myFactory:addMachine(mySequencedAssembler2)
+
 myFactory:scheduleJob({
+  {
+    "sequencedassembly",
+    {
+      depot = {"create:golden_sheet", 1},
+      [1] = {"create:cogwheel", 5},
+      [2] = {"create:large_cogwheel", 5},
+      [3] = {"minecraft:iron_nugget", 5},
+    }
+  },
   {
     "sequencedassembly",
     {
